30fb9a500aa70bf486219ac47ad97b5f6c3ce4e1
Matthew Augsburger <matthewaugsburger@gmail.com>
2017-08-22 08:58:31 -0500

    Added commenting

--- BanagazonWorkforceManager/Controllers/HomeController.cs
+++ BanagazonWorkforceManager/Controllers/HomeController.cs
@@ -20,25 +20,27 @@ namespace BanagazonWorkforceManager.Controllers
 
         public async Task<IActionResult> Index()
         {
+            // Creates an instance of the SplashView View Model
             var splash = new SplashView();
-
+            // Grabs all Training Programs and puts them in a list
             var trainingPrograms = await _context.TrainingProgram.ToListAsync();
-            var trainingProgramsWithinFour = new List<TrainingProgram>();
+            // Creates an intance to hold only training programs that are within 4 weeks of Datetime Now
+            var trainingProgramsWithinFourWeeks = new List<TrainingProgram>();
 
-
+            // Loops through all training programs in the database
             foreach (var tp in trainingPrograms)
             {
+                // Selects only training programs that start between Dattime Now and 28 days from Datetime Now
                 if(tp.StartDate <= DateTime.Now.AddDays(28) && tp.StartDate > DateTime.Now)
                 {
-                    trainingProgramsWithinFour.Add(tp);
+                    trainingProgramsWithinFourWeeks.Add(tp);
                 }
             }
-            splash.TrainingPrograms = trainingProgramsWithinFour;
-
+            // Sets the ViewModel object equal to the Selected List of Training Programs
+            splash.TrainingPrograms = trainingProgramsWithinFourWeeks;
+            // Grabs all the Employees from the database, Includes their department Orders by the Last added and takes only 5
             splash.Employees = await _context.Employee.Include(e => e.Department).OrderByDescending(e => e.StartDate).Take(5).ToListAsync();
-
-            
-
+            // Returns the View Model object to the razor template
             return View(splash);
         }
 
