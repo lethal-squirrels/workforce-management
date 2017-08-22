89134b533c7a890919c7cdd95992d0f99dd7a119
Matthew Augsburger <matthewaugsburger@gmail.com>
2017-08-22 02:15:33 -0500

    Functional needs commenting

--- BanagazonWorkforceManager/Controllers/HomeController.cs
+++ BanagazonWorkforceManager/Controllers/HomeController.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BanagazonWorkforceManager.Models;
+using Microsoft.EntityFrameworkCore;
+using BanagazonWorkforceManager.ViewModels;
 
 namespace BanagazonWorkforceManager.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly BanagazonWorkforceManagerContext _context;
+
+        public HomeController(BanagazonWorkforceManagerContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var splash = new SplashView();
+
+            var trainingPrograms = await _context.TrainingProgram.ToListAsync();
+            var trainingProgramsWithinFour = new List<TrainingProgram>();
+
+
+            foreach (var tp in trainingPrograms)
+            {
+                if(tp.StartDate <= DateTime.Now.AddDays(28) && tp.StartDate > DateTime.Now)
+                {
+                    trainingProgramsWithinFour.Add(tp);
+                }
+            }
+            splash.TrainingPrograms = trainingProgramsWithinFour;
+
+            splash.Employees = await _context.Employee.Include(e => e.Department).OrderByDescending(e => e.StartDate).Take(5).ToListAsync();
+
+            
+
+            return View(splash);
         }
 
         public IActionResult Error()
---
+++ BanagazonWorkforceManager/ViewModels/SplashView.cs
@@ -0,0 +1,26 @@
+﻿using BanagazonWorkforceManager.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BanagazonWorkforceManager.ViewModels
+{
+    public class SplashView
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public IEnumerable<Employee> Employees { get; set; }
+
+        public Department Department { get; set; }
+
+        [Display(Name = "Training Programs")]
+        public IEnumerable<TrainingProgram> TrainingPrograms { get; set; }
+
+    }
+}
